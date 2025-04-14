fc80d58560afb7e67bc89d513f9b40c0bd46f6c2
Miguel Rico Feo <miguelrico2031@gmail.com>
2025-04-14 14:16:06 +0200

    fixed removeButton func

--- LICENSE.md
+++ LICENSE.md
@@ -1,5 +1,6 @@
 ## MIT License
 
+Copyright (c) 2025 Miguel Rico Feo
 Copyright (c) 2022 Lukas Dürrenberger
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
--- src/ButtonManager.cpp
+++ src/ButtonManager.cpp
@@ -6,13 +6,13 @@ void ButtonManager::update(float dt)
 	bool mouseButtonDown = false;
 	if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
 	{
-		if(!wasMouseButtonPressed)
+		if (!wasMouseButtonPressed)
 			mouseButtonDown = true;
 		wasMouseButtonPressed = true;
 	}
 	else
 		wasMouseButtonPressed = false;
-	
+
 
 	sf::Vector2i mousePosInt = sf::Mouse::getPosition(m_window);
 	sf::Vector2f mousePos = { (float)mousePosInt.x, (float)mousePosInt.y };
@@ -75,11 +75,12 @@ Button& ButtonManager::addButton(std::function<void()> onClick, sf::String text,
 
 void ButtonManager::removeButton(Button& button)
 {
-	auto it = std::remove_if(m_buttons.begin(), m_buttons.end(), [&button](std::unique_ptr<Button>& b) { return b.get() == &button; });
+	auto it = std::remove_if(m_buttons.begin(), m_buttons.end(),
+		[&button](const std::unique_ptr<Button>& ptr) {	return ptr.get() == &button; });
+
 	if (it != m_buttons.end())
-	{
 		m_buttons.erase(it, m_buttons.end());
-	}
+
 }
 
 bool ButtonManager::isMouseOverButton(Button& button, sf::Vector2f mousePos)
